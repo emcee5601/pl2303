c7f3a21d2603428af7d72be87beb6ecf2a559823
Gerrit Niezen <info@embeddedloop.com>
2024-05-07 13:32:49 +0100

    not working yet

--- index.js
+++ index.js
@@ -8,15 +8,6 @@
  */
 
 import assert from 'assert';
-import { usb } from 'usb';
-
-const EventEmitter = require('events');
-
-function findDevices(vid, pid) {
-  return usb.getDeviceList()
-    .filter(device => device.deviceDescriptor.idVendor === vid
-      && device.deviceDescriptor.idProduct === pid);
-}
 
 const SupportedBaudrates = [
   75, 150, 300, 600, 1200, 1800, 2400, 3600,
@@ -25,118 +16,187 @@ const SupportedBaudrates = [
   921600, 1228800, 2457600, 3000000, 6000000,
 ];
 
-// find an endpoint of the given transfer type and direction
-function findEp(iface, transferType, direction) {
-  const eps = iface.endpoints.filter(e => e.transferType === transferType && e.direction === direction);
-  assert(eps.length === 1);
-  return eps[0];
+// // find an endpoint of the given transfer type and direction
+// function findEp(iface, transferType, direction) {
+//   const eps = iface.endpoints.filter(e => e.transferType === transferType && e.direction === direction);
+//   assert(eps.length === 1);
+//   return eps[0];
+// }
+
+// function controlTransfer(device, requestType, request, value, index, dataOrLength) {
+//   return new Promise((resolve, reject) => {
+//     device.controlTransfer(requestType, request, value, index, dataOrLength,
+//       (err, data) => {
+//         if (err) {
+//           reject(err);
+//           return;
+//         }
+//         resolve(data);
+//       });
+//   });
+// }
+
+async function vendorRead(device, value, index) {
+  const buffer = await device.controlTransferIn({
+    requestType: 'vendor',
+    recipient: 'device',
+    request: 0x01,
+    index,
+    value,
+  }, 1);
+
+  return buffer[0];
 }
 
-function controlTransfer(device, requestType, request, value, index, dataOrLength) {
-  return new Promise((resolve, reject) => {
-    device.controlTransfer(requestType, request, value, index, dataOrLength,
-      (err, data) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(data);
-      });
+async function vendorWrite(device, value, index) {
+  await device.controlTransferOut({
+    requestType: 'vendor',
+    recipient: 'device',
+    request: 0x01,
+    index,
+    value,
   });
 }
 
-function vendorRead(device, value, index) {
-  return controlTransfer(device, 0xc0, 0x01, value, index, 1)
-    .then(buffer => buffer[0]);
-}
-
-function vendorWrite(device, value, index) {
-  return controlTransfer(device, 0x40, 0x01, value, index, Buffer.alloc(0));
-}
-
-function setBaudrate(device, baud) {
+async function setBaudrate(device, baud) {
   assert(baud <= 115200);
   // find the nearest supported bitrate
   const list = SupportedBaudrates.slice().sort((a, b) => Math.abs(a - baud) - Math.abs(b - baud));
   const newBaud = list[0];
-  return controlTransfer(device, 0xa1, 0x21, 0, 0, 7)
-    .then((data) => {
-      const parameters = data;
-      parameters.writeInt32LE(newBaud, 0);
-      parameters[4] = 0; // 1 stop bit
-      parameters[5] = 0; // no parity
-      parameters[6] = 8; // 8 bit characters
-      return controlTransfer(device, 0x21, 0x20, 0, 0, parameters);
-    })
-    .then(() => vendorWrite(device, 0x0, 0x0)) // no flow control
-    .then(() => vendorWrite(device, 8, 0)) // reset upstream data pipes
-    .then(() => vendorWrite(device, 9, 0));
+  await device.controlTransferIn({
+    requestType: 'vendor',
+    recipient: 'device',
+    request: 0x01,
+    index: 0,
+    value: 0,
+  }, 7);
+  // device, 0xa1, 0x21, 0, 0, 7)
+
+  console.log('Setting baud rate to', newBaud);
+
+  const data = new ArrayBuffer(7);
+  const parameters = new DataView(data);
+  parameters.setInt32(0, newBaud, true);
+  parameters.setUint8(4, 0); // 1 stop bit
+  parameters.setUint8(5, 0); // no parity
+  parameters.setUint8(6, 8); // 8 bit characters
+  await device.controlTransferOut({
+    requestType: 'vendor',
+    recipient: 'device',
+    request: 0x01,
+    index: 0,
+    value: 0,
+  }, parameters);
+  // device, 0x21, 0x20, 0, 0, parameters);
+
+  await vendorWrite(device, 0x0, 0x0); // no flow control
+  await vendorWrite(device, 8, 0); // reset upstream data pipes
+  await vendorWrite(device, 9, 0);
 }
 
-export default class UsbSerial extends EventEmitter {
-  constructor(opts) {
+export default class UsbSerial extends EventTarget {
+  constructor(device, opts) {
     super();
-    const port = opts.port || 0;
     const bitrate = opts.baudRate || 9600;
-    const devices = findDevices(0x067b, 0x2303);
-    assert(devices.length > port);
-    const device = devices[port];
-    const descriptor = device.deviceDescriptor;
     this.device = device;
-    assert(descriptor.bDeviceClass !== 0x02);
-    assert(descriptor.bMaxPacketSize0 === 0x40); // HX type
-    device.timeout = 100;
-    device.open();
-    assert(device.interfaces.length === 1);
-    [this.iface] = device.interfaces;
-    this.iface.claim();
-    const intEp = findEp(this.iface, usb.LIBUSB_TRANSFER_TYPE_INTERRUPT, 'in');
-    intEp.on('data', (data) => {
-      this.emit('status', data);
+    assert(this.device.deviceClass !== 0x02);
+    this.device.open();
+    assert(this.device.configuration.interfaces.length === 1);
+
+    (async () => {
+      [this.iface] = this.device.configuration.interfaces;
+      console.log('Claiming interface', this.iface.interfaceNumber);
+      await this.device.claimInterface(this.iface.interfaceNumber);
+
+      // [this.inEndpoint, this.outEndpoint] = this.usbDevice.iface.alternate.endpoints;
+
+      // const intEp = findEp(this.iface, usb.LIBUSB_TRANSFER_TYPE_INTERRUPT, 'in');
+      // intEp.on('data', (data) => {
+      //   this.emit('status', data);
+      // });
+      // intEp.on('error', (err) => {
+      //   this.emit('error', err);
+      // });
+      // intEp.startPoll();
+      // const inEp = findEp(this.iface, usb.LIBUSB_TRANSFER_TYPE_BULK, 'in');
+      // inEp.on('data', (data) => {
+      //   this.emit('data', data);
+      // });
+      // inEp.on('error', (err) => {
+      //   this.emit('error', err);
+      // });
+      // const outEp = findEp(this.iface, usb.LIBUSB_TRANSFER_TYPE_BULK, 'out');
+      // outEp.on('error', (err) => {
+      //   this.emit('error', err);
+      // });
+      // this.outEp = outEp;
+      await vendorRead(this.device, 0x8484, 0);
+      await vendorWrite(this.device, 0x0404, 0);
+      await vendorRead(this.device, 0x8484, 0);
+      await vendorRead(this.device, 0x8383, 0);
+      await vendorRead(this.device, 0x8484, 0);
+      await vendorWrite(this.device, 0x0404, 1);
+      await vendorRead(this.device, 0x8484, 0);
+      await vendorRead(this.device, 0x8383, 0);
+      await vendorWrite(this.device, 0, 1);
+      await vendorWrite(this.device, 1, 0);
+      await vendorWrite(this.device, 2, 0x44);
+      await setBaudrate(this.device, bitrate);
+      // await inEp.startPoll())
+
+      this.isClosing = false;
+      this.readLoop();
+      this.dispatchEvent(new Event('ready'));
+    })().catch((error) => {
+      console.log('Error during PL2303 setup:', error);
+      this.dispatchEvent(new CustomEvent('error', {
+        detail: error,
+      }));
     });
-    intEp.on('error', (err) => {
-      this.emit('error', err);
-    });
-    intEp.startPoll();
-    const inEp = findEp(this.iface, usb.LIBUSB_TRANSFER_TYPE_BULK, 'in');
-    inEp.on('data', (data) => {
-      this.emit('data', data);
-    });
-    inEp.on('error', (err) => {
-      this.emit('error', err);
-    });
-    const outEp = findEp(this.iface, usb.LIBUSB_TRANSFER_TYPE_BULK, 'out');
-    outEp.on('error', (err) => {
-      this.emit('error', err);
-    });
-    this.outEp = outEp;
-    vendorRead(device, 0x8484, 0)
-      .then(() => vendorWrite(device, 0x0404, 0))
-      .then(() => vendorRead(device, 0x8484, 0))
-      .then(() => vendorRead(device, 0x8383, 0))
-      .then(() => vendorRead(device, 0x8484, 0))
-      .then(() => vendorWrite(device, 0x0404, 1))
-      .then(() => vendorRead(device, 0x8484, 0))
-      .then(() => vendorRead(device, 0x8383, 0))
-      .then(() => vendorWrite(device, 0, 1))
-      .then(() => vendorWrite(device, 1, 0))
-      .then(() => vendorWrite(device, 2, 0x44))
-      .then(() => setBaudrate(device, bitrate))
-      .then(() => inEp.startPoll())
-      .then(() => this.emit('ready'))
-      .catch(err => this.emit('error', err));
+  }
+
+  async readLoop() {
+    let result;
+
+    try {
+      result = await this.device.transferIn(1, 64);
+    } catch (error) {
+      if (error.message.indexOf('LIBUSB_TRANSFER_NO_DEVICE')) {
+        console.log('Device disconnected');
+      } else {
+        console.log('Error reading data:', error);
+      }
+    }
+
+    if (result && result.data && result.data.byteLength) {
+      console.log(`Received ${result.data.byteLength} byte(s).`);
+      const uint8buffer = new Uint8Array(result.data.buffer);
+      this.dispatchEvent(new CustomEvent('data', {
+        detail: uint8buffer.slice(0),
+      }));
+    }
+
+    if (!this.isClosing && this.device.opened) {
+      this.readLoop();
+    }
   }
 
   close(cb) {
-    this.removeAllListeners();
-    this.iface.release(true, () => {
-      this.device.close();
+    this.isClosing = true;
+    setTimeout(async () => {
+      await this.device.releaseInterface(0);
+      await this.device.close();
       return cb();
-    });
+    }, 2000);
   }
 
-  send(data) {
-    assert(data instanceof Buffer);
-    this.outEp.transfer(data);
+  // send(data) {
+  //   assert(data instanceof Buffer);
+  //   this.outEp.transfer(data);
+  // }
+  write(data, cb) {
+    this.device.transferOut(2, data).then(() => {
+      cb();
+    }, (err) => cb(err, null));
   }
 }
